2139de2d9fc48ba2986c9237f502e6d24e1b278c
chrisjuchem <chrisjuchem15@gmail.com>
2017-01-22 00:46:21 -0500

    credits

---
+++ GGJ_SoundWaves/Assets/Scripts/Credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+	public GameObject text;
+	public float scrollSpeed = 150;
+
+	// Use this for initialization
+	void Start () {
+		//text.transform.position.y -= 700;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.transform.position = text.transform.position + new Vector3 (0, scrollSpeed, 0) * Time.deltaTime;
+	}
+}
