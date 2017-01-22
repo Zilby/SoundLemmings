6fd3614364a9eacbc8f76e0c3b408aa737a94139
chrisjuchem <chrisjuchem15@gmail.com>
2017-01-22 11:11:58 -0500

    trailer level intro

---
+++ GGJ_SoundWaves/Assets/Scripts/trailerCam1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailerCam1 : MonoBehaviour {
+
+	public float zoomDelay = 4.5f;
+	public float zoomSpeed = 0.0f;
+	public float zoomAccel = 0.001f;
+
+	private float time = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (time > zoomDelay) {
+			zoomSpeed += zoomAccel;
+		}
+		time += Time.deltaTime;
+		gameObject.GetComponent<Camera> ().orthographicSize += zoomSpeed * Time.deltaTime;
+	}
+}
